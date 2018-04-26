a63b41b3746e0131a6fe98967c6e43fb68754bd6
Eyamin Hossain <eyamin.coder@gmail.com>
2018-04-26 16:36:10 +0600

    Directory name changes

--- lib/appbasic/urls.py
+++ lib/appbasic/urls.py
@@ -1,4 +1,4 @@
-"""portfolio URL Configuration
+"""appbasic URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
